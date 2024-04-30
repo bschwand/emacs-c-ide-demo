25f0a75177d1de850a1f0a849e2c40c22899f06a
bruno schwander <bruno.schwander@gmail.com>
2024-04-30 14:15:45 +0200

    little changes for me and to fix helm, which does not need config anymore
    Signed-off-by: bruno <bruno@bigboss.bschwand.net>

--- init.el
+++ init.el
@@ -1,3 +1,8 @@
+(setq-default cursor-type 'box)
+(set-cursor-color "red")
+
+(require 'subr-x)
+
 (require 'package)
 (add-to-list 'package-archives
          '("melpa" . "http://melpa.org/packages/") t)
@@ -19,12 +24,43 @@
 (if (version< emacs-version "24.4")
     (require 'setup-ivy-counsel)
   (require 'setup-helm)
-  (require 'setup-helm-gtags))
+  (require 'setup-helm-gtags)
+  )
+
 ;; (require 'setup-ggtags)
 (require 'setup-cedet)
 (require 'setup-editing)
 
+(projectile-global-mode)
+(setq projectile-completion-system 'helm)
+(helm-projectile-on)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+(setq projectile-indexing-method 'alien)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; PACKAGE: magit                       ;;
+;;                                      ;;
+;; GROUP: Programming -> Tools -> Magit ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'magit)
+(set-default 'magit-stage-all-confirm nil)
+(add-hook 'magit-mode-hook 'magit-load-config-extensions)
+
+;; full screen magit-status
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+(global-unset-key (kbd "C-x g"))
+(global-set-key (kbd "C-x g h") 'magit-log)
+(global-set-key (kbd "C-x g f") 'magit-file-log)
+(global-set-key (kbd "C-x g b") 'magit-blame-mode)
+(global-set-key (kbd "C-x g m") 'magit-branch-manager)
+(global-set-key (kbd "C-x g c") 'magit-branch)
+(global-set-key (kbd "C-x g s") 'magit-status)
+(global-set-key (kbd "C-x g r") 'magit-reflog)
+(global-set-key (kbd "C-x g t") 'magit-tag)
 
 ;; function-args
 ;; (require 'function-args)
@@ -36,9 +72,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-startup-truncated nil)
  '(package-selected-packages
-   (quote
-    (zygospore helm-gtags helm yasnippet ws-butler volatile-highlights use-package undo-tree iedit dtrt-indent counsel-projectile company clean-aindent-mode anzu))))
+   '(eglot helm-ag magit zygospore helm-gtags helm yasnippet ws-butler volatile-highlights use-package undo-tree iedit dtrt-indent counsel-projectile company clean-aindent-mode anzu)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
