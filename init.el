f7f4201a8d54d53cdf75121d0abf868bbc2d3645
bruno schwander <bruno.schwander@gmail.com>
2024-05-06 11:28:59 +0200

    add open file as other user command. great to edit config files as root

--- init.el
+++ init.el
@@ -81,3 +81,17 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(defun open-this-file-as-other-user (user)
+  "Edit current file as USER, using `tramp' and `sudo'.  If the current
+buffer is not visiting a file, prompt for a file name."
+  (interactive "sEdit as user (default: root): ")
+  (when (string= "" user)
+    (setq user "root"))
+  (let* ((filename (or buffer-file-name
+                       (read-file-name (format "Find file (as %s): "
+                                               user))))
+         (tramp-path (concat (format "/sudo:%s@localhost:" user) filename)))
+    (if buffer-file-name
+        (find-alternate-file tramp-path)
+      (find-file tramp-path))))
